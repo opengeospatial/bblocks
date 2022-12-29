fe8ba06b36b518e2bc0198cf22038e0ab864b5cb
doublebyte1 <info@doublebyte.net>
2022-12-29 11:23:25 +0000

    - add version 2.0 of registry

---
+++ assets/js/projects.js
@@ -0,0 +1,56 @@
+var projectSettings = {
+    "labels": [
+        {
+            "id": "geo",
+            "display_name": "geo",
+            "hex_color": "#bdb32f",
+            "hex_hover_color": "#cfc651",
+            "is_label_font_white": true
+        },        
+        {
+            "id": "ogc-utils",
+            "display_name": "ogc-utils",
+            "hex_color": "#1f97ed",
+            "hex_hover_color": "#74bef2",
+            "is_label_font_white": true
+        }, 
+
+
+        {
+            "id": "Valid",
+            "display_name": "valid",
+            "hex_color": "#178600",
+            "hex_hover_color": "#32a852",
+            "is_label_font_white": true
+        },
+        {
+            "id": "Proposal",
+            "display_name": "proposal",
+            "hex_color": "#c20034",
+            "hex_hover_color": "#871e3a",
+            "is_label_font_white": true
+        },
+        {
+            "id": "Mature",
+            "display_name": "mature",
+            "hex_color": "#2330e8",
+            "hex_hover_color": "#6b73ed",
+            "is_label_font_white": true
+        },
+        {
+            "id": "ogc-api-features",
+            "display_name": "ogc api features",
+            "hex_color": "#c97900",
+            "hex_hover_color": "#e8ad54",
+            "is_label_font_white": true
+        },        
+        {
+            "id": "ogc-api-edr",
+            "display_name": "ogc ap edr",
+            "hex_color": "#beffba",
+            "hex_hover_color": "#8bc487",
+            "is_label_font_white": false
+        },       
+
+    ]    
+}
